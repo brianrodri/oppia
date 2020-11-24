f258252a59aa531e9d8980df4ca2226ee8b78ac5
Brian Rodriguez <brian@brianrodri.com>
2020-11-24 07:44:07 -0500

    remove debug

--- scripts/common.py
+++ scripts/common.py
@@ -16,7 +16,6 @@
 
 from __future__ import absolute_import  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
-from __future__ import print_function
 
 import contextlib
 import getpass

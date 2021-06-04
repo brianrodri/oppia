041e2902fe4febd10d54b38df500a9ac1eff63c1
Vojtěch Jelínek <vojtin.j@gmail.com>
2021-06-04 20:20:06 +0200

    Fix tests

--- core/platform/storage/cloud_storage_emulator.py
+++ core/platform/storage/cloud_storage_emulator.py
@@ -114,6 +114,7 @@ class CloudStorageEmulator(python_utils.OBJECT):
         Returns:
             Blob. The blob.
         """
+        REDIS_CLIENT.wait(1, timeout=10000)
         blob_bytes = REDIS_CLIENT.get(filepath)
         return pickle.loads(blob_bytes) if blob_bytes is not None else None
 

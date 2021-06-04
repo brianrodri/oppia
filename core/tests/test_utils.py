c7c3c2f6af2a433c6b6f727783df9359714628f9
Vojtěch Jelínek <vojtin.j@gmail.com>
2021-06-04 23:58:49 +0200

    Debug tests

--- core/tests/test_utils.py
+++ core/tests/test_utils.py
@@ -1370,7 +1370,6 @@ class AppEngineTestBase(TestBase):
     def tearDown(self):
         datastore_services.delete_multi(
             datastore_services.query_everything().iter(keys_only=True))
-        storage_services.CLIENT.reset()
         super(AppEngineTestBase, self).tearDown()
 
     def run(self, result=None):

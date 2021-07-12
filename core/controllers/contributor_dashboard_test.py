239824fbdebe692c9ae1668515ccbff64acbcf61
Brian Rodriguez <brian@brianrodri.com>
2021-07-12 15:44:55 -0400

    fix broken tests

--- core/controllers/contributor_dashboard_test.py
+++ core/controllers/contributor_dashboard_test.py
@@ -215,7 +215,7 @@ class ContributionOpportunitiesHandlerTest(test_utils.GenericTestBase):
 
         self.assertEqual(
             response['opportunities'], [])
-        self.assertFalse(response['more'])
+        self.assertTrue(response['more'])
         self.assertTrue(
             isinstance(response['next_cursor'], python_utils.BASESTRING))
 

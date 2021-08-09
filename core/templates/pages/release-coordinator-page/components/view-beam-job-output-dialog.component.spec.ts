8164aa6a01aa5d6d15da1e59fc160b997a51cb1f
Brian Rodriguez <brian@brianrodri.com>
2021-08-09 04:01:50 -0400

    standard css

--- core/templates/pages/release-coordinator-page/components/view-beam-job-output-dialog.component.spec.ts
+++ core/templates/pages/release-coordinator-page/components/view-beam-job-output-dialog.component.spec.ts
@@ -32,9 +32,7 @@ import { BeamJobRun } from 'domain/jobs/beam-job-run.model';
 import { ViewBeamJobOutputDialogComponent } from 'pages/release-coordinator-page/components/view-beam-job-output-dialog.component';
 import { ReleaseCoordinatorBackendApiService } from 'pages/release-coordinator-page/services/release-coordinator-backend-api.service';
 import { AlertsService } from 'services/alerts.service';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
@@ -60,9 +58,7 @@ describe('View beam job output dialog', () => {
         MatButtonModule,
         MatCardModule,
         MatDialogModule,
-        MatFormFieldModule,
         MatIconModule,
-        MatInputModule,
         MatProgressBarModule,
         MatTabsModule,
         MatTooltipModule,

3443ec9deab1682cac857f6ecd524f07ecd55eec
Brian Rodriguez <brian@brianrodri.com>
2021-08-12 16:36:54 -0400

    add coverage

--- core/controllers/beam_jobs_test.py
+++ core/controllers/beam_jobs_test.py
@@ -18,12 +18,14 @@
 
 from __future__ import absolute_import
 from __future__ import unicode_literals
+import datetime
 
 from core.domain import beam_job_domain
 from core.domain import beam_job_services
 from core.tests import test_utils
 import feconf
 from jobs import base_jobs
+from jobs import jobs_manager
 
 import apache_beam as beam
 
@@ -88,3 +90,37 @@ class BeamJobRunHandlerTests(BeamHandlerTestBase):
         self.assertCountEqual([run['job_name'] for run in runs], ['FooJob'] * 3)
         self.assertCountEqual(
             [run['job_arguments'] for run in runs], [[], [], []])
+
+    def test_put_starts_new_job(self):
+        # type: () -> None
+        now = datetime.datetime.utcnow()
+        mock_job = beam_job_domain.BeamJobRun(
+            '123', 'FooJob', 'RUNNING', [], now, now, False)
+        run_job_sync_swap = self.swap_to_always_return(
+            jobs_manager, 'run_job_sync', value=mock_job)
+
+        with run_job_sync_swap:
+            response = self.put_json( # type: ignore[no-untyped-call]
+                '/beam_job_run', {'job_name': 'FooJob', 'job_arguments': []},
+                csrf_token=self.get_new_csrf_token()) # type: ignore[no-untyped-call]
+
+        self.assertEqual(response, mock_job.to_dict())
+
+
+class BeamJobRunResultHandlerTests(BeamHandlerTestBase):
+
+    def test_get_returns_job_output(self):
+        # type: () -> None
+        beam_job_services.create_beam_job_run_result_model('123', 'o', '').put()
+
+        response = self.get_json('/beam_job_run_result?job_id=123')
+
+        self.assertEqual(response, {'stdout': 'o', 'stderr': ''})
+
+    def test_get_raises_when_job_id_missing(self):
+        # type: () -> None
+        response = (
+            self.get_json('/beam_job_run_result', expected_status_int=400))
+
+        self.assertEqual(
+            response['error'], 'Missing key in handler args: job_id.')
